f6d1fccdb702520c8752811a463fa168051c3653
Altuğ Altuner <vefgend@gmail.com>
2024-04-07 16:24:49 +0300

    Functions added, responsiveness added

--- src/components/DishesMenu/DishesMenu.jsx
+++ src/components/DishesMenu/DishesMenu.jsx
@@ -10,15 +10,15 @@ import searchlogo from "../../assets/search.png";
 function DishesMenu() {
 
     const dishes = [
-        { id: 1, name: "Spicy seasoned seafood noodles", price: 2.29, imageSrc: photo1 },
-        { id: 2, name: "Salted Pasta with mushroom sauce", price: 2.70, imageSrc: photo2 },
-        { id: 3, name: "Beef dumpling in hot and sour soup", price: 2.96, imageSrc: photo3 },
-        { id: 4, name: "Healthy noodle with spinach leaf", price: 3.20, imageSrc: photo2 },
-        { id: 5, name: "Hot spicy fried rice with omelet", price: 3.05, imageSrc: photo1 },
-        { id: 6, name: "Spicy instant noodle with special omelette", price: 5.85, imageSrc: photo3 },
-        { id: 7, name: "Garlic Butter Shrimp Pasta", price: 4.52, imageSrc: photo3 },
-        { id: 8, name: "Rosemary Citrus Roasted Chicken", price: 4.80, imageSrc: photo1 },
-        { id: 9, name: "Smoky Barbecue Beef Brisket", price: 4.10, imageSrc: photo1 },
+        { id: 1, name: "Spicy seasoned seafood noodles", price: 2.29, imageSrc: photo1, coldDish: false, soup: false },
+        { id: 2, name: "Salted Pasta with mushroom sauce", price: 2.70, imageSrc: photo2, coldDish: false, soup: true },
+        { id: 3, name: "Beef dumpling in hot and sour soup", price: 2.96, imageSrc: photo3, coldDish: true, soup: false },
+        { id: 4, name: "Healthy noodle with spinach leaf", price: 3.20, imageSrc: photo2, coldDish: false, soup: true },
+        { id: 5, name: "Hot spicy fried rice with omelet", price: 3.05, imageSrc: photo1, coldDish: false, soup: false },
+        { id: 6, name: "Spicy instant noodle with special omelette", price: 5.85, imageSrc: photo3, coldDish: false, soup: false },
+        { id: 7, name: "Garlic Butter Shrimp Pasta", price: 4.52, imageSrc: photo3, coldDish: false, soup: true },
+        { id: 8, name: "Rosemary Citrus Roasted Chicken", price: 4.80, imageSrc: photo1, coldDish: true, soup: false },
+        { id: 9, name: "Smoky Barbecue Beef Brisket", price: 4.10, imageSrc: photo1, coldDish: true, soup: true },
         // Diğer yemeklerin bilgileri...
     ];
 
@@ -46,6 +46,25 @@ function DishesMenu() {
         }) + ', ' + date.toLocaleTimeString();
     };
 
+    const [activeTab, setActiveTab] = useState('hot-dishes');
+
+    function filterDishes(dish) {
+        if (activeTab === 'hot-dishes') {
+            return !dish.coldDish || dish.coldDish; //hepsi
+        } else if (activeTab === 'cold-dishes') {
+            return dish.coldDish && !dish.soup;
+        } else if (activeTab === 'soup') {
+            return !dish.coldDish && dish.soup;
+        } else if (activeTab === 'grill') {
+            return dish.coldDish && !dish.soup;
+        } else if (activeTab === 'appetizer') {
+            return !dish.coldDish && dish.soup;
+        } else if (activeTab === 'dessert') {
+            return !dish.coldDish || dish.coldDish;
+        }
+        return true;
+    }
+
     return (<div>
 
         <div className="username-and-search-segment">
@@ -59,6 +78,18 @@ function DishesMenu() {
             </div>
         </div>
 
+        <div className="category-tabs-dishes">
+            <div className="category-tabs">
+                <button className={activeTab === 'hot-dishes' ? 'active' : ''} onClick={() => setActiveTab('hot-dishes')}>Hot Dishes</button>
+                <button className={activeTab === 'cold-dishes' ? 'active' : ''} onClick={() => setActiveTab('cold-dishes')}>Cold Dishes</button>
+                <button className={activeTab === 'soup' ? 'active' : ''} onClick={() => setActiveTab('soup')}>Soup</button>
+                <button className={activeTab === 'grill' ? 'active' : ''} onClick={() => setActiveTab('grill')}>Grill</button>
+                <button className={activeTab === 'appetizer' ? 'active' : ''} onClick={() => setActiveTab('appetizer')}>Appetizer</button>
+                <button className={activeTab === 'dessert' ? 'active' : ''} onClick={() => setActiveTab('dessert')}>Dessert</button>
+            </div>
+
+        </div>
+
         <div className="dishes-main">
             <div className="top-segment-for-dishes">
                 <h2 className="dishes-title">Choose Dishes</h2>
@@ -70,13 +101,17 @@ function DishesMenu() {
                 </select>
             </div>
             <div className="dishes-container">
-                {dishes.map(dish => (
-                    <div className="dish-card" key={dish.id}>
-                        <img className="dish-image" src={dish.imageSrc} alt={dish.name} />
-                        <h3 className="dish-name">{dish.name}</h3>
-                        <p className="dish-price">${dish.price}</p>
-                    </div>
-                ))}
+                {/* dishes.filter(filterDishes): dishes dizisindeki her bir dish öğesini filterDishes
+                fonksiyonuna gönderir. filterDishes fonksiyonu, aktif tab'a göre bir yemeğin koşulları karşılayıp karşılamadığını kontrol eder.Koşulları karşılayan (true dönen) yemekler yeni bir dizi içinde bir araya getirilir.  .map(dish => { ... }): Filtrelenmiş dizi üzerinde iterasyon yaparak, her bir dish öğesi için JSX kod bloğu oluşturur. Oluşturulan bu JSX kod blokları, kullanıcı arayüzünde ilgili yemeklerin gösterilmesini sağlar. */}
+                {
+                    dishes.filter(filterDishes).map(dish => (
+                        <div className="dish-card" key={dish.id}>
+                            <img className="dish-image" src={dish.imageSrc} alt={dish.name} />
+                            <h3 className="dish-name">{dish.name}</h3>
+                            <p className="dish-price">${dish.price}</p>
+                        </div>
+                    ))
+                }
             </div>
 
         </div>
