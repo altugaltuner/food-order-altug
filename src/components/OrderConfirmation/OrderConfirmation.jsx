1a157f773b679db3b363e3e5cd685f4ae8b0bced
Altuğ Altuner <vefgend@gmail.com>
2024-04-08 14:40:29 +0300

    Update OrderConfirmation.jsx

--- src/components/OrderConfirmation/OrderConfirmation.jsx
+++ src/components/OrderConfirmation/OrderConfirmation.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./OrderConfirmation.scss";
 
+let PaymentOption = "option1";
+
+const handleOptionChange = (e) => {
+    PaymentOption = e.target.value;
+    console.log(PaymentOption);
+};
+
 function OrderConfirmation() {
     return (
         <div>
@@ -9,20 +16,19 @@ function OrderConfirmation() {
                 <label htmlFor="option1" className={`payment-photo-options ${PaymentOption === "option1" ? "selected" : ""}`}>
                     <input className="input-type-radio" type="radio" id="option1" name="options" value="option1" checked={PaymentOption === "option1"}
                         onChange={handleOptionChange} />
-                    <img src={cCard} alt="" srcSet="" />
-                    Credit Card
+                    Dine In
                 </label>
+
                 <label htmlFor="option2" className={`payment-photo-options ${PaymentOption === "option2" ? "selected" : ""}`}>
                     <input className="input-type-radio" type="radio" id="option2" name="options" value="option2" checked={PaymentOption === "option2"}
                         onChange={handleOptionChange} />
-                    <img src={paypalPhoto} alt="" srcSet="" />
-                    Paypal
+                    To Go
                 </label>
+
                 <label htmlFor="option3" className={`payment-photo-options ${PaymentOption === "option3" ? "selected" : ""}`}>
                     <input className="input-type-radio" type="radio" id="option3" name="options" value="option3" checked={PaymentOption === "option3"}
                         onChange={handleOptionChange} />
-                    <img src={cashPhoto} alt="" srcSet="" />
-                    Cash
+                    Delivery
                 </label>
 
             </div>
