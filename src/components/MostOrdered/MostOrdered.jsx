2953c74d1d7421e84640fa0f7bd04c58ba8ac399
Altuğ Altuner <vefgend@gmail.com>
2024-04-10 14:50:10 +0300

    functions deleted

--- src/components/MostOrdered/MostOrdered.jsx
+++ src/components/MostOrdered/MostOrdered.jsx
@@ -3,7 +3,6 @@ import "./MostOrdered.scss";
 
 function MostOrdered() {
 
-    let discountPercentage = 20;
 
     // foodItems artık bir state
     const [foodItems, setFoodItems] = useState([
@@ -12,59 +11,6 @@ function MostOrdered() {
         { id: 3, name: "Beef Dumpling in hot and sour soup", price: 2.99, imageSrc: "../src/assets/food1.png", quantity: 0, totalPrice: 0, OrderNote: "" },
     ]);
 
-    // isNaN fonksiyonu, bir değerin NaN olup olmadığını kontrol eder. Eğer parametre olarak verilen değer NaN ise, true döner; aksi halde false döner.
-    // ! operatörü, isNaN fonksiyonunun sonucunu tersine çevirir. Yani, eğer parsedQuantity NaN değilse, !isNaN(parsedQuantity) ifadesi true olur.
-    const handlePieceChange = (id, value) => {
-        setFoodItems(foodItems.map(item => {
-            if (item.id === id) {
-                const parsedQuantity = parseInt(value, 10);
-                const quantity = !isNaN(parsedQuantity) ? parsedQuantity : 0;
-                return { ...item, quantity, totalPrice: quantity * item.price };
-            }
-            return item;
-        }));
-    };
-
-    function subTotal() {
-        let total = 0;
-        for (const element of foodItems) {
-            total += element.totalPrice;
-        }
-        let discountMoney = 100 - discountPercentage;
-        total = (total * (discountMoney / 100));
-        return total;
-    };
-
-    function discountedMoney() {
-        let total = 0;
-        for (const element of foodItems) {
-            total += element.totalPrice;
-        }
-        let discountMoney = total * (discountPercentage / 100);
-        return discountMoney;
-    };
-
-    const handleCustomerNoteChange = (id, note) => {
-        setFoodItems(foodItems.map(item => {
-            if (item.id === id) {
-                return { ...item, OrderNote: note };
-            }
-            return item;
-        }));
-        console.log(foodItems);
-        //burada notedaki sıkıntı son harfi almıyor!!!
-    };
-
-    // Silme işlevi : DeleteMeal Fonksiyonu: deleteMeal fonksiyonu, aldığı id değerine göre hangi yemeğin silineceğini anlar. Fonksiyon, filter metoduyla foodItems dizisinde dolaşır ve her bir öğenin id değeri ile fonksiyona argüman olarak verilen id değerini karşılaştırır. Eşleşmeyen (yani silinmesi gerekmeyen) öğeler yeni bir dizi oluşturmak üzere kullanılır. Eşleşen öğe (silinmesi gereken yemek) bu yeni dizide yer almaz.
-    const deleteMeal = (id) => {
-        const newFoodItems = foodItems.filter(item => item.id !== id);
-        setFoodItems(newFoodItems);
-    };
-
-
-    const addingButtonImg = "../src/assets/adding-button.png";
-    const dustbin = "../src/assets/dustbin-logo.png";
-
 
     return (
         <div>
@@ -72,15 +18,10 @@ function MostOrdered() {
 
                 <div className="title-order-conf">
                     <div className="part-of-title-conf">
-                        <h1 className="h1-order-conf">Confirmation</h1>
-                        <h3 className="h3-order-conf">Orders #34562</h3>
-                    </div>
-                    <div className="part-of-title-conf">
-                        <img src={addingButtonImg} alt="" srcset="" className="adding-button-class" />
+                        <h1 className="h1-order-conf">Most Ordered</h1>
+                        <button className="button-order">asasas</button>
                     </div>
                 </div>
-
-                {/* Map Metodu: foodItems.map(...) kullandığınızda, foodItems dizisindeki her bir öğe için bir döngü oluşturursunuz. Bu döngü içinde, her öğenin kendine ait bir div bloğu (ve içinde çöp kutusu butonu) oluşturulur. Her bir div ve içerdiği buton, o anki döngüdeki öğenin (foodItem) verileriyle (özellikle id değeriyle) ilişkilendirilmiş olur. */}
 
                 {foodItems.map((foodItem) => (
                     <div className="order-conf-body" key={foodItem.id}>
@@ -94,38 +35,17 @@ function MostOrdered() {
                                         <p className="name-and-price-p">${foodItem.price.toFixed(2)}</p>
                                     </div>
                                 </div>
-                                <input type="text" className="total-piece-of-food" onChange={(e) => handlePieceChange(foodItem.id, e.target.value)}
-                                    value={foodItem.quantity || ''}
-                                />
 
                             </div>
 
-
-                            <input type="text" onChange={(e) => handleCustomerNoteChange(foodItem.id, e.target.value)} className="food-customer-note" placeholder="Order note..." value={foodItem.OrderNote} />
                         </div>
 
-                        {/* OnClick Olayı: Çöp kutusu butonuna tıklandığında tetiklenen onClick olayında, deleteMeal(foodItem.id) ifadesi kullanılır. Bu ifade, tıklanan butonun ait olduğu foodItem öğesinin id değerini deleteMeal fonksiyonuna argüman olarak geçirir. */}
-
-                        <div className="conf-right-order-part">
-                            <p className="price-of-order-conf">${foodItem.totalPrice.toFixed(2)}</p>
-                            <button type="reset" className="reset-button-conf" onClick={() => deleteMeal(foodItem.id)}><img src={dustbin} alt="Reset" className="reset-button-conf-img" /></button>
-                        </div>
                     </div>
                 ))}
 
+                <button className="view-all-orders">View All</button>
 
                 <div className="discount-subtotal-div-line"></div>
-
-                <div className="discount-subtotal-div">
-                    <div className="discount-subtotal-div-line">
-                        <p className="discount-subtotal-p">Discount : %{discountPercentage}</p>
-                        <p className="discount-subtotal-p">-$ {discountedMoney()}</p>
-                    </div>
-                    <div className="discount-subtotal-div-line">
-                        <p className="discount-subtotal-p">Sub total :</p>
-                        <p className="discount-subtotal-p">$ {subTotal().toFixed(2)}</p>
-                    </div>
-                </div>
 
             </div>
         </div>
