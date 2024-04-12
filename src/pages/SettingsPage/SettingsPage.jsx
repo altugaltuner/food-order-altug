3f94c814188f8b43522deb256e85caf1dd298135
Altuğ Altuner <vefgend@gmail.com>
2024-04-12 14:32:16 +0300

    Update SettingsPage.jsx

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -1,10 +1,12 @@
 import "./SettingsPage.scss";
 import "../../components/ProductsManagement/ProductsManagement";
 import ProductsManagement from "../../components/ProductsManagement/ProductsManagement";
+import Navbar from "../../components/Navbar/Navbar";
 
 function SettingsPage() {
   return (
     <main className="settings-page">
+      <Navbar />
       <ProductsManagement />
     </main>
   );
