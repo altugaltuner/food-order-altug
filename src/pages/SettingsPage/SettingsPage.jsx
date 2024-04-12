4f28ff188727e72a65414e62c2862642d82fbb8c
Altuğ Altuner <vefgend@gmail.com>
2024-04-12 14:37:13 +0300

    Completed merge

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -2,12 +2,14 @@ import "./SettingsPage.scss";
 import "../../components/ProductsManagement/ProductsManagement";
 import ProductsManagement from "../../components/ProductsManagement/ProductsManagement";
 import Navbar from "../../components/Navbar/Navbar";
+import SettingsSidebar from "../../components/SettingsSidebar/SettingsSidebar";
 
 function SettingsPage() {
   return (
     <main className="settings-page">
       <Navbar />
       <ProductsManagement />
+      <SettingsSidebar />
     </main>
   );
 }
