f0c4e4ac6d1110ecae15b29b1f044adb302005f4
Altuğ Altuner <vefgend@gmail.com>
2024-04-12 12:06:46 +0300

    2 branch has been merged succesfully

--- src/components/DishesMenu/DishesMenu.jsx
+++ src/components/DishesMenu/DishesMenu.jsx
@@ -11,14 +11,14 @@ function DishesMenu() {
 
     const dishes = [
         { id: 1, name: "Spicy seasoned seafood noodles", price: 2.29, imageSrc: photo1, coldDish: false, soup: false },
-        { id: 2, name: "Salted Pasta with mushroom sauce", price: 2.70, imageSrc: photo2, coldDish: false, soup: true },
+        { id: 2, name: "Salted Pasta with mushroom sauce", price: 2.71, imageSrc: photo2, coldDish: false, soup: true },
         { id: 3, name: "Beef dumpling in hot and sour soup", price: 2.96, imageSrc: photo3, coldDish: true, soup: false },
-        { id: 4, name: "Healthy noodle with spinach leaf", price: 3.20, imageSrc: photo2, coldDish: false, soup: true },
+        { id: 4, name: "Healthy noodle with spinach leaf", price: 3.28, imageSrc: photo2, coldDish: false, soup: true },
         { id: 5, name: "Hot spicy fried rice with omelet", price: 3.05, imageSrc: photo1, coldDish: false, soup: false },
         { id: 6, name: "Spicy instant noodle with special omelette", price: 5.85, imageSrc: photo3, coldDish: false, soup: false },
         { id: 7, name: "Garlic Butter Shrimp Pasta", price: 4.52, imageSrc: photo3, coldDish: false, soup: true },
-        { id: 8, name: "Rosemary Citrus Roasted Chicken", price: 4.80, imageSrc: photo1, coldDish: true, soup: false },
-        { id: 9, name: "Smoky Barbecue Beef Brisket", price: 4.10, imageSrc: photo1, coldDish: true, soup: true },
+        { id: 8, name: "Rosemary Citrus Roasted Chicken", price: 4.85, imageSrc: photo1, coldDish: true, soup: false },
+        { id: 9, name: "Smoky Barbecue Beef Brisket", price: 4.19, imageSrc: photo1, coldDish: true, soup: true },
         // Diğer yemeklerin bilgileri...
     ];
 
