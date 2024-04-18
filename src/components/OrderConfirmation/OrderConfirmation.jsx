9c9fa9f7ae0de7f8e2b75587764c31ef8e65914f
Altuğ Altuner <vefgend@gmail.com>
2024-04-18 15:28:13 +0300

    responsive of homepage is fixed

--- src/components/OrderConfirmation/OrderConfirmation.jsx
+++ src/components/OrderConfirmation/OrderConfirmation.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import "./OrderConfirmation.scss";
 
-
 function OrderConfirmation({
   incomingFoodItems,
   deleteMeal,
@@ -65,20 +64,20 @@ function OrderConfirmation({
   }
 
   return (
-    <div className="order-conf-main">
+    <div className="order-conff-main">
       <div className="title-order-conf">
         <div className="part-of-title-conf">
           <h1 className="h1-order-conf">Confirmation</h1>
           <h3 className="h3-order-conf">Orders #34562</h3>
         </div>
-        <div className="part-of-title-conf">
-          <img
-            src={addingButtonImg}
-            alt=""
-            srcset=""
-            className="adding-button-class"
-          />
-        </div>
+
+        <img
+          src={addingButtonImg}
+          alt=""
+          srcset=""
+          className="adding-button-class"
+        />
+
       </div>
 
       {/* Map Metodu: foodItems.map(...) kullandığınızda, foodItems dizisindeki her bir öğe için bir döngü oluşturursunuz. Bu döngü içinde, her öğenin kendine ait bir div bloğu (ve içinde çöp kutusu butonu) oluşturulur. Her bir div ve içerdiği buton, o anki döngüdeki öğenin (foodItem) verileriyle (özellikle id değeriyle) ilişkilendirilmiş olur. */}
@@ -93,7 +92,7 @@ function OrderConfirmation({
                   alt=""
                   className="food-photos-order-conf"
                 />
-                <div className="name-and-price">
+                <div className="name-and-price-div">
                   <p className="name-and-price-p">{foodItem.name}</p>
                   <p className="name-and-price-p">${foodItem.price}</p>
                 </div>
